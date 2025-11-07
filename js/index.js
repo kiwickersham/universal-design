9f81f1f71e72a1bd059c2df619a986231a71301a
kiwickersham <94425658+kiwickersham@users.noreply.github.com>
2025-11-07 02:13:24 -0700

    project portal checkpoint

---
+++ js/index.js
@@ -0,0 +1,23 @@
+function l2enter() {
+    document.getElementById("lab2").innerText = "Lab #2 - Coding & Posting a Web Page - The Basics";
+}
+
+function l2leave() {
+    document.getElementById("lab2").innerText = "Lab #2";
+}
+
+function l3enter() {
+    document.getElementById("lab3").innerText = "Lab #3 - Tables, Forms, Tabbed Browsing, and Screen Reading";
+}
+
+function l3leave() {
+    document.getElementById("lab3").innerText = "Lab #3";
+}
+
+function l4enter() {
+    document.getElementById("lab4").innerText = "Lab #4 - Accessible CSS Layout";
+}
+
+function l4leave() {
+    document.getElementById("lab4").innerText = "Lab #4";
+}
